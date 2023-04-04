3f1a46012fc79639ff700d32649f7c8e60210cbb
ji-hoooon <ljh6384@naver.com>
2023-04-04 12:21:13 +0900

    ADD 리포지토리 추가 (for 회원가입)

--- src/main/java/com/mino/bank/domain/account/AccountRepository.java
+++ src/main/java/com/mino/bank/domain/account/AccountRepository.java
@@ -1,6 +1,7 @@
 package com.mino.bank.domain.account;
 
 import org.hibernate.metamodel.model.convert.spi.JpaAttributeConverter;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AccountRepository extends JpaAttributeConverter<Account, Long> {
+public interface AccountRepository extends JpaRepository<Account, Long> {
 }
