6bd152427f6bae5f13012ce4106d97c1947b03c4
ji-hoooon <ljh6384@naver.com>
2023-04-04 12:22:59 +0900

    ADD 유저 컨트롤러, 서비스 추가 (for 회원가입)

---
+++ src/main/java/com/mino/bank/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.mino.bank.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+}
---
+++ src/main/java/com/mino/bank/service/UserService.java
@@ -0,0 +1,11 @@
+package com.mino.bank.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+}
