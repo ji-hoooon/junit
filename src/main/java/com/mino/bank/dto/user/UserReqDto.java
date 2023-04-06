21380aaecb6bb011d82801bf8eebb876c06ff1ff
ji-hoooon <ljh6384@naver.com>
2023-04-07 00:34:09 +0900

    ADD 정규표현식을 이용한 유효성검사 완료

--- src/main/java/com/mino/bank/dto/user/UserReqDto.java
+++ src/main/java/com/mino/bank/dto/user/UserReqDto.java
@@ -7,6 +7,8 @@ import lombok.Setter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 
 public class UserReqDto {
@@ -15,12 +17,23 @@ public class UserReqDto {
     @Getter
     @Setter
     public static class JoinReqDto {
+        //영문, 숫자만 가능하고, 길이는 2~20자만 가능하도록, 공백도 불가능
+        @Pattern(regexp = "^[A-Za-z]{2,20}$\"",message = "영문/숫자 2~20자 이내로 작성해 주세요.")
         @NotEmpty
         private String username;
+
+        //길이 4~20만 가능
+        @Size(min=4, max=20) //String에만 사용가능한 어노테이션
         @NotEmpty
         private String password;
+
+        //이메일 형식을 준수하도록
+        @Pattern(regexp = "^[A-Za-z0-9]{2,10}@[A-Za-z0-9]{2,6}\\.[a-zA-Z]{2,3}$",message = "이메일 형식으로 작성해 주세요.")
         @NotEmpty
         private String email;
+
+        //영어, 한글, 길이는 1~20자만 가능하도록
+        @Pattern(regexp = "^[A-Za-z가-힣]{1,20}$",message = "영문/한글 1~20자 이내로 작성해 주세요.")
         @NotEmpty
         private String fullname;
 
---
+++ src/test/java/com/mino/bank/temp/RegexTest.java
@@ -0,0 +1,133 @@
+package com.mino.bank.temp;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.regex.Pattern;
+
+public class RegexTest {
+
+    //길이는 +,*,{start, end}
+    //: +는 빈문자열 불가능, *은 빈문자열도 가능
+    @Test
+    public void 한글만된다_test() throws Exception{
+        //given
+        String value="가나";
+//        boolean result = Pattern.matches("^[ㄱ-ㅎ가-힣]$", value);
+        //한글로 시작(^)하거나 한글로 끝나($)는 것
+//        boolean result = Pattern.matches("^[ㄱ-ㅎ가-힣]*$", value);
+        //한글로 시작(^)하거나 한글로 끝나($)는 것+ 무한으로 반복 가능(*), 하지만 빈 문자열도 true
+        boolean result = Pattern.matches("^[ㄱ-ㅎ가-힣]+$", value);
+        //한글로 시작(^)하거나 한글로 끝나($)는 것+ 무한으로 반복 가능(*), 한 글자 이상 꼭 필요
+
+        System.out.println("테스트 : "+result);
+        //빈 regex에 공백이면 참
+
+    }
+    @Test
+    public void 한글안됨_test() throws Exception{
+        //given
+        String value=" ";
+//        boolean result = Pattern.matches("^[^ㄱ-ㅎ가-힣]+$", value);
+        // !(한글로 시작(^)하거나 한글로 끝나($)는 것+ 무한으로 반복 가능(*)), 한글이 아닌 문자 한 글자 이상 꼭 필요
+        boolean result = Pattern.matches("^[^ㄱ-ㅎ가-힣]*$", value);
+        // !(한글로 시작(^)하거나 한글로 끝나($)는 것+ 무한으로 반복 가능(*)), 빈 문자열도 가능
+
+        System.out.println("테스트 : "+result);
+
+
+    }
+    @Test
+    public void 영어만된다_test() throws Exception{
+        //given
+        String value="가나";
+//        boolean result = Pattern.matches("^[A-Za-z]$", value);
+        //영어로 시작(^)하거나 영어로 끝나($)는 것
+//        boolean result = Pattern.matches("^[A-Za-z]*$", value);
+        //영어로 시작(^)하거나 영어로 끝나($)는 것+ 무한으로 반복 가능(*), 하지만 빈 문자열도 true
+        boolean result = Pattern.matches("^[A-Za-z]+$", value);
+        //영어로 시작(^)하거나 영어로 끝나($)는 것+ 무한으로 반복 가능(*), 한 글자 이상 꼭 필요
+
+        System.out.println("테스트 : "+result);
+        //빈 regex에 공백이면 참
+
+    }
+    @Test
+    public void 영어는안된다_test() throws Exception{
+        //given
+        String value="가11";
+//        boolean result = Pattern.matches("^[^A-Za-z]+$", value);
+        // !(영어로 시작(^)하거나 영어로 끝나($)는 것+ 무한으로 반복 가능(*)), 영어이 아닌 문자 한 글자 이상 꼭 필요
+        boolean result = Pattern.matches("^[^A-Za-z]*$", value);
+        // !(영어로 시작(^)하거나 영어로 끝나($)는 것+ 무한으로 반복 가능(*)), 빈 문자열도 가능
+
+        System.out.println("테스트 : "+result);
+
+
+    }
+    @Test
+    public void 영어와숫자만된다_test() throws Exception{
+        //given
+        String value = "가11";
+        boolean result = Pattern.matches("^[0-9A-Za-z]+$", value);
+
+        System.out.println("테스트 : "+result);
+
+    }
+    @Test
+    public void 영어만되고_길이는최소2최대4이다_test() throws Exception{
+        //given
+        String value="가나";
+
+        boolean result = Pattern.matches("^[A-Za-z]{2,4}$", value);
+        //영어로 시작(^)하거나 영어로 끝나($)는 것+ 무한으로 반복 가능(*), 길이는 2,4만 가능
+
+        System.out.println("테스트 : "+result);
+        //빈 regex에 공백이면 참
+
+
+    }
+    @Test
+    public void 영어숫자특수문자만되고_공백불가능_길이는최소4최대12이다_test() throws Exception{
+        //given
+        String value="a#(sasd(12";
+
+        boolean result = Pattern.matches("^[a-zA-Z0-9!@#$%^&*()_+\\-=\\[\\]{};:\\\\|,.<>\\/?]{4,12}$", value);
+        //영어와 숫자,특수문자로 시작(^)하거나 영어와 숫자,특수문자로 끝나($)는 것+ 무한으로 반복 가능(*), 길이는 4,12만 가능
+
+        System.out.println("테스트 : "+result);
+        //빈 regex에 공백이면 참
+
+
+    }
+
+    //username, email, fullname 테스트 진행
+    @Test
+    //영문,숫자, 2~20자이내
+    public void user_username_test() throws Exception{
+        //given
+        String username = "ssar";
+        boolean result = Pattern.matches("^[A-Za-z]{2,20}$", username);
+
+        System.out.println("테스트 : "+result);
+    }
+    @Test
+    //영문,숫자, 2~20자이내
+    public void user_email_test() throws Exception{
+        //given
+        String email = "ssar@nate.com";
+        //com만 가능한 테스트
+        //메타문자의 경우 \\를 추가해 메타문자가 아닌걸 표현해야한다.
+        boolean result = Pattern.matches("^[A-Za-z0-9]{2,10}@[A-Za-z0-9]{2,6}\\.[a-zA-Z]{2,3}$", email);
+
+        System.out.println("테스트 : "+result);
+    }
+    @Test
+    //영문,한글, 1~20자이내
+    public void user_fullname_test() throws Exception{
+        //given
+        String fullname = "ssar";
+        boolean result = Pattern.matches("^[A-Za-z가-힣]{1,20}$", fullname);
+
+        System.out.println("테스트 : "+result);
+    }
+}
