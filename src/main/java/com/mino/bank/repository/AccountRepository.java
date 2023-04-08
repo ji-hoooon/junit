f7043ab58d26ce7bb9704cc97303421cb13656a5
ji-hoooon <ljh6384@naver.com>
2023-04-09 01:37:34 +0900

    ADD 내 계좌 목록 조회 서비스 완성

--- src/main/java/com/mino/bank/repository/AccountRepository.java
+++ src/main/java/com/mino/bank/repository/AccountRepository.java
@@ -3,6 +3,7 @@ package com.mino.bank.repository;
 import com.mino.bank.domain.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
@@ -15,4 +16,8 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     //select * from account where number = :number
     //checkpoint: 리팩토링 필요 -> user 객체가 지연로딩이므로 함께 땡겨오도록 fetch join?
     Optional<Account> findByNumber(Long number);
+
+    //로그인한 유저의 계좌 목록만 보는 메서드
+    //select * from account where user_id=:id;
+    List<Account> findByUser_id(Long id);
 }
--- src/main/java/com/mino/bank/service/AccountService.java
+++ src/main/java/com/mino/bank/service/AccountService.java
@@ -7,11 +7,16 @@ import com.mino.bank.dto.account.AccountRespDto.AccountSaveRespDto;
 import com.mino.bank.handler.ex.CustomApiException;
 import com.mino.bank.repository.AccountRepository;
 import com.mino.bank.repository.UserRepository;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -51,5 +56,73 @@ public class AccountService {
         //: AccountSaveRespDto의 생성자로 Account를 전달해 생성
         return new AccountSaveRespDto(accountPS);
     }
+
+    public AccountListRespDto 계좌목록보기_유저별(Long userId){
+        //(1) userId로 DB에서 유저 조회
+        User userPS = userRepository.findById(userId).orElseThrow(
+                () -> new CustomApiException("유저를 찾을 수 없습니다.")
+        );
+        //(2) 찾은 유저의 id를 이용해서 계좌 목록 조회
+        List<Account> accountListPS = accountRepository.findByUser_id(userPS.getId());
+
+        //(3) 찾은 계좌 목록을 응답하기 위한 DTO 작성
+        return new AccountListRespDto(userPS, accountListPS);
+    }
+
+    @Getter
+    @Setter
+    public static class AccountListRespDto{
+        private String fullname;
+        private List<AccountDto> accounts= new ArrayList<>();
+
+        //여러 계좌마다 가지고 있는 User 객체를 각각 조회하면, 매우 많은 리소스 낭비
+        //: Account가 가지고 있는 User 객체는 LAZY 로딩하지 않는다.
+        //따라서 유저 조회 1번, 계좌 목록 조회 1번으로 DB I/O를 최소화한다.
+        public AccountListRespDto(User user, List<Account> accounts) {
+            this.fullname = user.getFullname();
+            //Account 리스트 -> 각각 new AccountDto해서 리스트로 변환
+
+            //(1) 스트림
+//            this.accounts = accounts
+//                    //스트림 생성 메서드
+//                    .stream()
+//                    //변환 - 중간 연산
+//                    .map(
+//                            (account)-> new AccountDto(account)
+//                    )
+//                    //리스트로 모음 - 최종 연산
+//                    .collect(Collectors.toList());
+
+            //(2) 메서드 참조
+            this.accounts = accounts
+                    //스트림 생성 메서드
+                    .stream()
+                    //변환 - 중간 연산
+                    .map(
+                            AccountDto::new
+                    )
+                    //리스트로 모음 - 최종 연산
+                    .collect(Collectors.toList());
+        }
+
+
+        //서비스에서 반드시 컨트롤러한테 엔티티가 아닌 DTO로 변환해서 응답해야 한다.
+        //컨트롤러로 응답할 내용 : id, number, balance
+        @Getter
+        @Setter
+        public class AccountDto{
+            private Long id;
+            private Long number;
+            private Long balance;
+
+            //생성자로 Account를 받아서 DTO로 변환
+            public AccountDto(Account account) {
+                this.id = account.getId();
+                this.number = account.getNumber();
+                this.balance = account.getBalance();
+            }
+        }
+    }
+
 
 }
