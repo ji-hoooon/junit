17aaead85f80f9083eab8835b2044bbc1fba7adb
ji-hoooon <ljh6384@naver.com>
2023-04-09 20:12:36 +0900

    UPDATE 토큰 테스트의 만료시간 잠재적 에러 해결

--- src/test/java/com/mino/bank/config/jwt/JwtProcessTest.java
+++ src/test/java/com/mino/bank/config/jwt/JwtProcessTest.java
@@ -30,11 +30,31 @@ class JwtProcessTest {
         assertThat(jwtToken.startsWith(JwtVO.TOKEN_PREFIX));
     }
 
-    @Test
+    //테스트에서 사용하는 토큰 반환하는 메서드
+    private String createToken() {
+        //given
+        //(1) 테스트에 사용할 유저 객체 생성
+        User user = User.builder()
+                .id(1L)
+                .role(UserEnum.CUSTOMER)
+                .build();
+        LoginUser loginUser = new LoginUser(user);
+
+        //when
+        //(2) 테스트 진행하기 위해 토큰 반환
+        return JwtProcess.create(loginUser);
+    }
+
+        @Test
     void verify_test() {
         //given
-        //(1) 토큰 검증을 위해 Bearer를 제외한 토큰 값을 가져옴
-        String jwtToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJiYW5rIiwicm9sZSI6IkNVU1RPTUVSIiwiaWQiOjEsImV4cCI6MTY4MTUzNzg1M30.8yhovOveayIJN-bCnFKx7ucQRbP0FVH8gLo9tD9a0HG0F2PKZu1cl6RWazwtMVi59ENh_Krve1xQGqFMBBANXA";
+        String jwtToken = createToken();
+//        jwtToken=jwtToken.substring(7);
+        jwtToken=jwtToken.replace(JwtVO.TOKEN_PREFIX, "");
+
+            //(1) 토큰 검증을 위해 Bearer를 제외한 토큰 값을 가져옴
+//        String jwtToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJiYW5rIiwicm9sZSI6IkNVU1RPTUVSIiwiaWQiOjEsImV4cCI6MTY4MTUzNzg1M30.8yhovOveayIJN-bCnFKx7ucQRbP0FVH8gLo9tD9a0HG0F2PKZu1cl6RWazwtMVi59ENh_Krve1xQGqFMBBANXA";
+        //: 토큰값을 이렇게 명시적으로 준다면, 만료시간이 이후에 사용 불가능
 
         //when
         //(2) 토큰 검증 후, 리턴값을 LoginUser 객체로 저장
