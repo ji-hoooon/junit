a67387855dbb3b23002f59907f25bbf294728b07
ji-hoooon <ljh6384@naver.com>
2023-04-10 12:28:59 +0900

    ADD 입출금내역 동적 쿼리 작성

--- src/main/java/com/mino/bank/repository/TransactionRepository.java
+++ src/main/java/com/mino/bank/repository/TransactionRepository.java
@@ -3,5 +3,9 @@ package com.mino.bank.repository;
 import com.mino.bank.domain.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-}
+//동적쿼리를 위한 DAO와 JpaRepository 상속
+//public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+public interface TransactionRepository extends JpaRepository<Transaction, Long>, Dao {
+    //인터페이스가 인터페이스를 구현할 때에는 implements 키워드 대신 extends 키워드를 사용하여 상속을 표현
+    //인터페이스 끼리 상속할 때는 구현부가 없기 떄문에 extends 사용
+ }
---
+++ src/main/java/com/mino/bank/repository/TransactionRepositoryImpl.java
@@ -0,0 +1,75 @@
+package com.mino.bank.repository;
+
+import com.mino.bank.domain.Transaction;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.query.Param;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+//동적쿼리를 위한 DAO
+interface Dao{
+    //파라미터 하나면 @Param이 필요없지만
+    //파라미터 여러개면 어노테이션이 필요하다.
+    List<Transaction> findTransactionList(@Param("accountId") Long accountId, String gubun, @Param("page") Integer page);
+}
+
+@RequiredArgsConstructor
+//인터페이스를 구현한 클래스
+public class TransactionRepositoryImpl implements Dao{
+    private final EntityManager em;
+
+    /**
+     * 1. JPQL이란
+     * 2. join fetch란
+     * 3. outer join이란
+     * @param accountId
+     * @param gubun
+     * @param page
+     * @return
+     */
+
+    @Override
+    public List<Transaction> findTransactionList(Long accountId, String gubun, Integer page) {
+        //gubun값을 이용해서 동적쿼리 작성한다.
+
+        //JPQL으로 쿼리 작성
+        String sql="";
+        sql +="select t from Transaction t ";
+
+        //fetch join
+        if(gubun.equals("WITHDRAW")){
+            sql+="join fetch t.withdrawAccount wa ";
+            sql+="where t.withdrawAccount.id =: withdrawAccountId ";
+        }else if(gubun.equals("DEPOSIT")){
+            sql+="join fetch t.depositAccount da ";
+            sql+="where t.depositAccount.id =: depositAccountId ";
+        }else{  //gubun=ALL
+            //입출력 : 아우터 조인 필요
+            sql+="left join t.withdrawAccount wa ";
+            sql+="left join t.withdrawAccount da ";
+            sql+="where t.depositAccount.id =: depositAccountId ";
+            sql+="or ";
+            sql+= "t.withdrawAccount.id =: withdrawAccountId ";
+        }
+
+        //createQuery - JPQL, createNativeQuery - SQL
+        TypedQuery<Transaction> query=em.createQuery(sql, Transaction.class);
+
+        if(gubun.equals("WITHDRAW")){
+            query=query.setParameter("withdrawAccountId", accountId);
+        }
+        else if(gubun.equals("DEPOSIT")){
+            query=query.setParameter("depositAccountId", accountId);
+        }else{
+            query=query.setParameter("withdrawAccountId", accountId);
+            query=query.setParameter("depositAccountId", accountId);
+        }
+        query.setFirstResult(page*5);
+        //page = 0, 0, 5, 10, -> 5, 10, 15
+        query.setMaxResults(5);
+
+        return query.getResultList();
+    }
+}
